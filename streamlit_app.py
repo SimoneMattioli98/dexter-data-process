cc69ab531c62d50790ed75d45cefa7806b8e9f41
Simone Mattioli <64487424+SimoneMattioli98@users.noreply.github.com>
2024-04-01 20:03:05 +0000

    remove unwanted prints

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,16 @@ import altair as alt
 import streamlit as st
 import matplotlib.pyplot as plt
 # Page title
-st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
-st.title('📊 Interactive Data Explorer')
+st.set_page_config(page_title='Dexter data processing', page_icon='📊')
+st.title('📊 Dexter data processing')
 
-with st.expander('About this app'):
-  st.markdown('**What can this app do?**')
-  st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
-  st.markdown('**How to use the app?**')
-  st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
+#with st.expander('About this app'):
+#  st.markdown('**What can this app do?**')
+#  st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
+#  st.markdown('**How to use the app?**')
+#  st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
   
-st.subheader('Which Movie Genre performs ($) best at the box office?')
+#st.subheader('Select a csv file with data...')
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
